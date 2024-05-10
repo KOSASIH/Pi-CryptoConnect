c34ffc89e31394010badaf25dcb7aa0a856c1995
KOSASIH <kosasihg88@gmail.com>
2024-05-10 17:44:30 +0700

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,32 @@
+# config/__init__.py
+
+from .settings import (
+    DEBUG,
+    SECRET_KEY,
+    DATABASE_URL,
+    API_KEY,
+)
+
+# Additional configuration options
+class Config:
+    @staticmethod
+    def init_app(app):
+        # Register blueprints
+        from . import blueprints
+        app.register_blueprint(blueprints.crypto_bp)
+        app.register_blueprint(blueprints.data_bp)
+
+        # Configure logging
+        if not app.debug:
+            import logging
+            from logging.handlers import RotatingFileHandler
+
+            file_handler = RotatingFileHandler("app.log", maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+            app.logger.setLevel(logging.INFO)
+            app.logger.info("Application startup")
+
+        return None
