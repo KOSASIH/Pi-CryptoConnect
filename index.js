bc1a28705b224e40925342f95200adec4bd26785
KOSASIH <kosasihg88@gmail.com>
2024-05-10 16:29:35 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './store/reducers';
+import { App } from './App';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
