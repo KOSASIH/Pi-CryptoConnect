18308bbd254f787d50e0ccec1693fd717f77ab6b
KOSASIH <kosasihg88@gmail.com>
2024-05-15 00:50:46 +0700

    Create indodax.py

---
+++ exchanges/indodax.py
@@ -0,0 +1,14 @@
+# exchanges/indodax.py
+import ccxt
+
+class Indodax:
+    """Indodax exchange implementation."""
+
+    def __init__(self):
+        """Initialize Indodax exchange."""
+        self.exchange = ccxt.indodax()
+
+    def get_symbols(self) -> List[str]:
+        """Get available symbols on Indodax."""
+        symbols = self.exchange.load_markets()
+        return list(symbols.keys())
