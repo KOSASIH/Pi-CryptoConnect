ae3ae261774894329fc6cea246c7cf0cfe07130e
KOSASIH <kosasihg88@gmail.com>
2024-05-15 00:37:55 +0700

    Create exchange_utils.py

---
+++ exchanges/exchange_utils.py
@@ -0,0 +1,7 @@
+# exchanges/exchange_utils.py
+from typing import Dict
+
+def get_exchange_config(exchange_name: str) -> Dict[str, str]:
+    """Get exchange configuration from environment variables."""
+    # implementation
+    pass
