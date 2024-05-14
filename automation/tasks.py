c6079757cfa3ab12b1fc713ece4fca4357f0ed43
KOSASIH <kosasihg88@gmail.com>
2024-05-15 03:23:15 +0700

    Create tasks.py

---
+++ automation/tasks.py
@@ -0,0 +1,30 @@
+# tasks.py
+from celery import Celery
+from time import sleep
+import RPi.GPIO as GPIO
+
+# Initialize Celery
+celery = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+# Set up GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+@celery.task
+def toggle_gpio():
+    GPIO.output(18, not GPIO.input(18))
+    sleep(1)
+
+# celery.py
+from celery import Celery
+
+# Initialize Celery
+celery = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+# Set up periodic tasks
+celery.conf.beat_schedule = {
+    'toggle-gpio-every-30-seconds': {
+        'task': 'tasks.toggle_gpio',
+        'schedule': 30.0,
+    },
+}
