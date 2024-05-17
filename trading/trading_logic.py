1a5be570bd0c26a00d12aef0ba1dd050d04dc8f8
KOSASIH <kosasihg88@gmail.com>
2024-05-17 08:23:38 +0700

    Create trading_logic.py

---
+++ trading/trading_logic.py
@@ -0,0 +1,12 @@
+from services import CurrencyService
+
+currency_service = CurrencyService()
+
+def execute_trade(user_id, amount, price, currency_symbol):
+    currency = currency_service.get_currency_by_symbol(currency_symbol)
+    if currency:
+        trade = Trade(user_id=user_id, amount=amount, price=price, currency_id=currency.id)
+        db.session.add(trade)
+        db.session.commit()
+        return True
+    return False
