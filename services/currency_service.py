7f76c1e5e642ca3360bf683ff85d7daa0fe4d0f8
KOSASIH <kosasihg88@gmail.com>
2024-05-17 08:20:45 +0700

    Create currency_service.py

---
+++ services/currency_service.py
@@ -0,0 +1,19 @@
+from models import Currency
+
+class CurrencyService:
+    def get_all_currencies(self):
+        return Currency.query.all()
+
+    def get_currency_by_id(self, currency_id):
+        return Currency.query.get(currency_id)
+
+    def get_currency_by_symbol(self, symbol):
+        return Currency.query.filter_by(symbol=symbol).first()
+
+    def update_exchange_rate(self, currency_id, new_exchange_rate):
+        currency = self.get_currency_by_id(currency_id)
+        if currency:
+            currency.exchange_rate = new_exchange_rate
+            db.session.commit()
+            return True
+        return False
