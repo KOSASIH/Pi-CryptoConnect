6ca66e50e5cf6ffb38e5326103645d56bc5e34c2
KOSASIH <kosasihg88@gmail.com>
2024-05-17 08:22:42 +0700

    Create trade.py

---
+++ models/trade.py
@@ -0,0 +1,13 @@
+from models import Currency
+
+class Trade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'))
+    amount = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    currency = db.relationship('Currency', backref='trades')
+
+    def __repr__(self):
+        return f"Trade('{self.user_id}', {self.amount}, {self.price}, {self.currency.symbol})"
