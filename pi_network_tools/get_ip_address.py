eb3fcde7dcf4f36972baac712f351b39c03a9f25
KOSASIH <kosasihg88@gmail.com>
2024-05-17 07:51:26 +0700

    Create get_ip_address.py

---
+++ pi_network_tools/get_ip_address.py
@@ -0,0 +1,11 @@
+import socket
+
+def get_ip_address():
+    """
+    Get the IP address of the Raspberry Pi.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
