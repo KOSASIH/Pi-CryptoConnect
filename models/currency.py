ebd1bee1cf027f54f8a34bf14b374dc85e309ae3
KOSASIH <kosasihg88@gmail.com>
2024-05-17 08:21:50 +0700

    Create currency.py

---
+++ models/currency.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Currency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    symbol = db.Column(db.String(10), nullable=False)
+    exchange_rate = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Currency('{self.name}', '{self.symbol}', {self.exchange_rate})"
