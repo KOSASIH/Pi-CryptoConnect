f4396911a9958a2cccd92d3d850a8374ed2aa4a1
KOSASIH <kosasihg88@gmail.com>
2025-02-10 13:41:20 +0700

    Update pi_coin_lister.py

--- pi_coin_price_manager/pi_coin_lister.py
+++ pi_coin_price_manager/pi_coin_lister.py
@@ -1,19 +1,19 @@
 import requests
 import time
 
-def set_pi_coin_price(price):
-    # Set the Pi coin price on global exchanges
-    url = "https://api.globalexchanges.com/set_price"
+def set_pi_coin_value(value):
+    # Set the Pi coin value on global exchanges
+    url = "https://api.globalexchanges.com/set_value"
     headers = {"Content-Type": "application/json"}
-    data = {"price": price, "coin": "Pi"}
+    data = {"value": value, "coin": "Pi", "type": "stablecoin"}
     response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
-        print("Pi coin price set to:", price)
+        print("Pi coin value set to:", value)
     else:
-        print("Failed to set Pi coin price:", response.text)
+        print("Failed to set Pi coin value:", response.text)
 
-# Set the Pi coin price to $314.159
-set_pi_coin_price(314.159)
+# Set the Pi coin value to $314,159.00
+set_pi_coin_value(314159.00)
 
-# You can also set a delay between price updates
+# You can also set a delay between value updates
 # time.sleep(60)  # Delay for 60 seconds
