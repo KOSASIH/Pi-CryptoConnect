f1db7ff965804ed8bea9fdc90149fc05fafc68a2
KOSASIH <kosasihg88@gmail.com>
2025-03-30 11:30:07 +0700

    Update BitcoinService.js

--- blockchain/BitcoinService.js
+++ blockchain/BitcoinService.js
@@ -1,9 +1,10 @@
-import Bitcoin from 'bitcoinjs-lib';
+import * as Bitcoin from 'bitcoinjs-lib';
 import { BitcoinConfig } from '../config/BitcoinConfig';
+import axios from 'axios';
 
 class BitcoinService {
-  private network: any;
-  private keyPair: any;
+  private network: Bitcoin.Network;
+  private keyPair: Bitcoin.ECPairInterface;
 
   constructor() {
     this.network = Bitcoin.networks[BitcoinConfig.NETWORK];
@@ -12,10 +13,9 @@ class BitcoinService {
 
   async getAccountBalance(): Promise<number> {
     try {
-      const blockchain = new Bitcoin.Blockchain.Blockchain(this.network);
       const address = this.keyPair.getAddress();
-      const balance = await blockchain.getBalance(address);
-      return balance;
+      const response = await axios.get(`https://blockchain.info/q/addressbalance/${address}?confirmations=6`);
+      return response.data / 100000000; // Convert satoshis to BTC
     } catch (error) {
       throw new Error(`Error fetching account balance: ${error.message}`);
     }
@@ -23,19 +23,66 @@ class BitcoinService {
 
   async sendTransaction(toAddress: string, value: number): Promise<string> {
     try {
-      const transaction = new Bitcoin.TransactionBuilder(this.network);
       const fromAddress = this.keyPair.getAddress();
-      const rawTransaction = transaction
-        .addInput(fromAddress, 0)
-        .addOutput(toAddress, value)
-        .buildIncomplete()
-        .sign(this.keyPair);
+      const unspentOutputs = await this.getUnspentOutputs(fromAddress);
+      const transaction = new Bitcoin.TransactionBuilder(this.network);
 
-      const sender = new Bitcoin.Wallet(this.keyPair, { network: this.network });
-      const transactionData = sender.broadcast(rawTransaction);
-      return transactionData.toString('hex');
+      let totalInputValue = 0;
+      for (const output of unspentOutputs) {
+        transaction.addInput(output.txid, output.vout);
+        totalInputValue += output.value;
+        if (totalInputValue >= value) break; // Stop if we have enough funds
+      }
+
+      if (totalInputValue < value) {
+        throw new Error('Insufficient funds for transaction.');
+      }
+
+      transaction.addOutput(toAddress, value);
+      const change = totalInputValue - value - 1000; // Subtract fee (1000 satoshis)
+      if (change > 0) {
+        transaction.addOutput(fromAddress, change); // Send change back to sender
+      }
+
+      const rawTransaction = transaction.buildIncomplete();
+      const transactionHex = rawTransaction.toHex();
+      const signedTransaction = await this.signTransaction(rawTransaction);
+      const transactionHash = await this.broadcastTransaction(signedTransaction);
+      return transactionHash;
     } catch (error) {
       throw new Error(`Error sending transaction: ${error.message}`);
+    }
+  }
+
+  private async getUnspentOutputs(address: string): Promise<any[]> {
+    try {
+      const response = await axios.get(`https://blockchain.info/unspent?active=${address}`);
+      return response.data.unspent_outputs;
+    } catch (error) {
+      throw new Error(`Error fetching unspent outputs: ${error.message}`);
+    }
+  }
+
+  private async signTransaction(transaction: Bitcoin.Transaction): Promise<string> {
+    const txb = Bitcoin.TransactionBuilder.fromTransaction(transaction, this.network);
+    const fromAddress = this.keyPair.getAddress();
+    const keyPair = this.keyPair;
+
+    for (let i = 0; i < txb.tx.ins.length; i++) {
+      txb.sign(i, keyPair);
+    }
+
+    return txb.build().toHex();
+  }
+
+  private async broadcastTransaction(transactionHex: string): Promise<string> {
+    try {
+      const response = await axios.post('https://api.blockcypher.com/v1/btc/main/txs/push', {
+        tx: transactionHex,
+      });
+      return response.data.tx.hash;
+    } catch (error) {
+      throw new Error(`Error broadcasting transaction: ${error.message}`);
     }
   }
 }
