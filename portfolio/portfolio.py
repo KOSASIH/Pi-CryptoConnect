dd64bb27214687934dab769e39a0268dd9313527
KOSASIH <kosasihg88@gmail.com>
2025-03-31 13:36:36 +0700

    Update portfolio.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from pi_cryptoconnect.database import db
 from pi_cryptoconnect.models import User, Portfolio, Asset
 
@@ -12,7 +12,7 @@ def portfolio_index():
     :return: Portfolio index page
     """
     portfolios = Portfolio.query.all()
-    return render_template('portfolio/index.html', portfolios[portfolios])
+    return render_template('portfolio/index.html', portfolios=portfolios)
 
 @portfolio_blueprint.route('/portfolio/create', methods=['GET', 'POST'])
 def create_portfolio():
@@ -21,14 +21,25 @@ def create_portfolio():
 
     :return: Redirect to portfolio index page
     """
+    if 'user_id' not in session:
+        flash('You need to be logged in to create a portfolio.')
+        return redirect(url_for('auth.login'))  # Redirect to login if not authenticated
+
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
-        user_id = request.user.id
+        user_id = session['user_id']  # Get user ID from session
         portfolio = Portfolio(name=name, description=description, user_id=user_id)
-        db.session.add(portfolio)
-        db.session.commit()
-        return redirect(url_for('portfolio.portfolio_index'))
+        
+        try:
+            db.session.add(portfolio)
+            db.session.commit()
+            flash('Portfolio created successfully!')
+            return redirect(url_for('portfolio.portfolio_index'))
+        except Exception as e:
+            db.session.rollback()  # Rollback in case of error
+            flash('Error creating portfolio: ' + str(e))
+
     return render_template('portfolio/create.html')
 
 @portfolio_blueprint.route('/portfolio/<int:portfolio_id>', methods=['GET'])
